abdb2ca1b4f2daa61a44c57be2cf82a39451daa9
kanghyun98 <lkh981107@gmail.com>
2021-12-01 20:53:53 +0900

    update fixed files

--- week-2/이강현_김민주_김서연/app.js
+++ week-2/이강현_김민주_김서연/app.js
@@ -1,42 +1,50 @@
 const problemList = [
   {
-    title: '문제 1',
-    list: ['1', '2', '3'],
-    answer: 2,
+    title: '세상에서 제일 멋진 사람은?',
+    list: ['고광서', '김민주', '이종헌'],
+    answer: 1,
   },
   {
-    title: '문제 2',
-    list: ['1', '2', '3'],
-    answer: 2,
+    title: '세상에서 제일 똑똑한 사람은?',
+    list: ['김서연', '공소나', '고광서'],
+    answer: 0,
   },
   {
-    title: '문제 3',
-    list: ['1', '2', '3'],
+    title: '세상에서 제일 개쩌는 사람은?',
+    list: ['공소나', '이종헌', '이강현'],
     answer: 2,
   },
 ];
-let scoreBoard = [1, 2, 3];
+let scoreBoard = [];
 let nowPage = 0;
 let finalScore = 0;
 
 function nextProblems() {
-  saveAnswer(nowPage);
+  if (saveAnswer(nowPage)) {
+    return;
+  }
   try {
     deleteProblems();
   } catch (error) {}
   nowPage += 1;
-  if (nowPage >= problemList.length) {
+  if (nowPage == problemList.length) {
     score();
+  } else if (nowPage > problemList.length) {
+    location.reload(true);
   }
   insertProblems(nowPage);
 }
 function saveAnswer() {
-  var obj_len = document.getElementsByName('checkValue').length;
-  console.log(obj_len);
+  const obj_len = document.getElementsByName('checkValue').length;
+  let flag = false;
   for (var i = 0; i < obj_len; i++) {
     if (document.getElementsByName('checkValue')[i].checked == true) {
       scoreBoard[nowPage] = i;
+      flag = true;
     }
+  }
+  if (!flag) {
+    return true;
   }
 }
 function deleteProblems() {
@@ -66,7 +74,8 @@ function score() {
     }
   }
   const title = document.getElementById('title');
-  title.innerHTML = '당신의 점수는' + finalScore + '점입니다';
+  document.querySelector('.submit').innerText = 'Replay';
+  title.innerHTML = '당신의 점수는 ' + finalScore + '점입니다';
 }
 
 function submitBtn() {
