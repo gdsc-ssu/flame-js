b2b08b52096139b59efd3373a24f33bf0ab3f181
이강현 <lkh981107@gmail.com>
2021-12-13 03:00:40 +0900

    fix: Solve week2 project's errors and update codes (#3)
    * fix: Solve errors and change some codes
    
    * docs: update README.md

--- week-2/이강현_김민주_김서연/app.js
+++ week-2/이강현_김민주_김서연/app.js
@@ -15,72 +15,92 @@ const problemList = [
     answer: 2,
   },
 ];
+
 let scoreBoard = [];
 let nowPage = 0;
 let finalScore = 0;
+let wrongProblems = 0;
 
 function nextProblems() {
-  if (saveAnswer(nowPage)) {
+  if (!saveAnswer(nowPage)) {
     return;
   }
-  try {
-    deleteProblems();
-  } catch (error) {}
+
   nowPage += 1;
+  changeProblems(nowPage);
+
   if (nowPage == problemList.length) {
     score();
-  } else if (nowPage > problemList.length) {
-    location.reload(true);
   }
-  insertProblems(nowPage);
 }
-function saveAnswer() {
-  const obj_len = document.getElementsByName('checkValue').length;
+
+// 답을 기록하는 함수
+function saveAnswer(nowPage) {
+  const $inputRadioTags = document.getElementsByName('checkValue');
+  const obj_len = $inputRadioTags.length;
   let flag = false;
-  for (var i = 0; i < obj_len; i++) {
-    if (document.getElementsByName('checkValue')[i].checked == true) {
+
+  for (let i = 0; i < obj_len; i++) {
+    if ($inputRadioTags[i].checked === true) {
       scoreBoard[nowPage] = i;
       flag = true;
     }
   }
-  if (!flag) {
-    return true;
-  }
-}
-function deleteProblems() {
-  document.querySelectorAll('#num' + nowPage).forEach((element) => {
-    element.remove();
-  });
-}
-function insertProblems(index) {
-  const title = document.getElementById('title');
-  const psList = document.getElementById('psList');
-  title.innerHTML = problemList[index].title;
 
+  return flag;
+}
+
+// 문제 및 보기 변경 함수
+function changeProblems(index) {
+  if (!problemList[index]) {
+    return;
+  }
+
+  const $title = document.getElementById('title');
+  const $psList = document.getElementById('psList');
+
+  $title.innerHTML = problemList[index].title; // 제목 변경
+  $psList.innerHTML = ''; // 보기 변경
   problemList[nowPage].list.forEach((element) => {
-    psList.innerHTML +=
-      ' <li id=num' +
-      nowPage +
-      '><input type="radio" name="checkValue" class="problem-value problem1"/><label for="problem">' +
-      element +
-      '</label></li>';
+    $psList.innerHTML += `<li id=num${nowPage}>
+        <input type="radio" name="checkValue" class="problem-value problem1"/><label for="problem">
+      ${element}
+      </label></li>
+      `;
   });
 }
+
+// 결과 반환 함수
+function returnResult(score, wrong) {
+  const $title = document.getElementById('title');
+  const $submit = document.querySelector('.submit');
+  const $psList = document.getElementById('psList');
+
+  $title.innerHTML = `${wrong}개 틀리셨네요, 점수는 ${Math.floor(
+    (100 / problemList.length) * score
+  )}점입니다`;
+  $psList.innerHTML = '';
+  $submit.innerText = 'Replay';
+  $submit.addEventListener('click', () => window.location.reload(true));
+}
+
+// 점수 채점 함수
 function score() {
   for (let i = 0; i < problemList.length; i++) {
     if (problemList[i].answer === scoreBoard[i]) {
       finalScore += 1;
     } else {
+      wrongProblems += 1;
     }
   }
-  const title = document.getElementById('title');
-  document.querySelector('.submit').innerText = 'Replay';
-  title.innerHTML = '당신의 점수는 ' + finalScore + '점입니다';
+
+  returnResult(finalScore, wrongProblems);
 }
 
 function submitBtn() {
   nextProblems();
 }
+
 window.onload = function () {
-  insertProblems(0);
+  changeProblems(0);
 };
