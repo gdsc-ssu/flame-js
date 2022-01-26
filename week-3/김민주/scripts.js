6ec1ef4ecd80d2ed95544971dfdc1ebf7749377d
Kim Minju <39041161+Margarets00@users.noreply.github.com>
2022-01-26 20:28:08 +0900

    20220105 minju_kim notes app (#6)

--- week-3/김민주/scripts.js
+++ week-3/김민주/scripts.js
@@ -1,38 +1,110 @@
 
+let notesData = JSON.parse(localStorage.getItem('saveData')) || [
+    {"contents":"","noteId":"abcdefg","createdAt":new Date().toLocaleString(),"color":"#123123"}
+] 
+const DEBOUNCE_DURATION = 300; // in 
 
 
 function addNotes (){
-    const boards = document.getElementById('boards');
     const noteId = Math.random().toString(36).substr(2,11);
+    let randomColor = generateRandomHexColor();
+    notesData.push({"contents":"","noteId":noteId,"createdAt":new Date().toLocaleString(),"color":randomColor})
 
-    boards.innerHTML += `<div class="notes-body" id=${noteId}>
-    <header><button id="deleteBtn" onclick="deleteNotes.call(this)">X</button>
-    <button id="saveBtn" onclick="saveNotes.call(this)">save</button></header>
-    <textarea class="notes-contents"">Hi I am Note</textarea>
-                        </div>
-    `;
+    getNotes()
     
 }
 function deleteNotes(){
-    this.parentNode.parentNode.remove()
+    const selector = this.parentNode.parentNode;
+    const selectorIndex = notesData.findIndex(x => x?.noteId === selector.id);
+
+    notesData.splice(selectorIndex,1)
+    localStorage.setItem('saveData',JSON.stringify(notesData))
+    //console.log(selector.id)
+    getNotes()
 }
 
-function editNotes(){
-    
-}
-function saveNotes(){
+function getNotes(){
     const boards = document.getElementById('boards');
-    localStorage.setItem('saveData',boards.innerHTML,)
-    //var doc = new DOMParser().parseFromString(str, "text/html")
-    console.log(boards.innerHTML)
-    //console.log(this.parentNode.parentNode.parentNode.innerHTML)
+    boards.innerHTML = ''
+
+    notesData.forEach(index => {
+        boards.innerHTML += `<div class="notes-body" style="background-color:${index?.color}" id=${index?.noteId}>
+    <header class="notes-header"><p class="date" style="color:${contrast(index?.color)}">${index?.createdAt}</p><button id="deleteBtn" onclick="deleteNotes.call(this)"><i style="color:#db5d5d" class="fa fa-times"></i></button></header>
+    <textarea style="color:${contrast(index?.color)}" oninput="onInputDebounced({id: this.parentNode.id, value:this.value, arg1: 'someArg2'})"  class="notes-contents" id="textarea" placeholder="please enter the contents" >${index.contents}</textarea>
+                        </div>
+    `;
+    randomColor = generateRandomHexColor(); 
+    });
 }
+
 window.onload = function () {
-    const boards = document.getElementById('boards');
-    try{
-        boards.innerHTML += localStorage.getItem('saveData')
-    }
-    catch(e){
-
-    }
+    getNotes()
   };
+
+
+function onInputChange({id, value, arg1}){
+    const selectorIndex = notesData.findIndex(x => x?.noteId === id);
+    notesData[selectorIndex].contents = value
+    localStorage.setItem('saveData',JSON.stringify(notesData))
+}
+
+const onInputDebounced = debounce( ({id, value, arg1}) => {
+onInputChange({id, value, arg1}); 
+}, DEBOUNCE_DURATION);
+
+function debounce(callback, wait, context = this) {
+    let timeout = null;
+    let callbackArgs = null;
+
+    const later = () => callback.apply(context, callbackArgs);
+
+    return function({id}) {
+        callbackArgs = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
+
+function rgbToYIQ({r, g, b}) {
+    return ((r * 299) + (g * 587) + (b * 114)) / 1000;
+  }
+
+  function hexToRgb(hex) {
+    if (!hex || hex === undefined || hex === '') {
+      return undefined;
+    }
+
+    const result =
+          /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : undefined;
+  }
+
+  function contrast(colorHex, threshold = 128) {
+    if (colorHex === undefined) {
+      return '#000';
+    }
+
+    const rgb = hexToRgb(colorHex);
+
+    if (rgb === undefined) {
+      return '#000';
+    }
+
+    return rgbToYIQ(rgb) >= threshold ? '#000' : '#fff';
+  }
+
+  function generateRandomHexColor (){
+    const hexadecimalIntegers = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+    let hexColor = "#"
+  
+    while (hexColor.length < 7) {
+      hexColor += hexadecimalIntegers[ Math.floor( Math.random() * hexadecimalIntegers.length ) ]
+    }
+    return hexColor
+  }
