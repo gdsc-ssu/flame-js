038db188ca19154b912fe85a345d2c4d8c1764d0
kanghyun98 <lkh981107@gmail.com>
2022-01-27 00:23:10 +0900

    fix kanghyun's github profile project

--- week-6/kanghyun/script.js
+++ week-6/kanghyun/script.js
@@ -7,17 +7,21 @@ const $searchInput = document.querySelector('.search-form__input');
 const GITHUB_SEARCH_URL = 'https://api.github.com/users/';
 
 const getSearchData = async (name) => {
-  const searchURL = GITHUB_SEARCH_URL + name;
-  const reposURL = searchURL + '/repos';
+  try {
+    const searchURL = GITHUB_SEARCH_URL + name;
+    const reposURL = searchURL + '/repos';
 
-  const searchRes = await fetch(searchURL);
-  const searchData = await searchRes.json();
+    const searchRes = await fetch(searchURL);
+    const searchData = await searchRes.json();
 
-  const reposRes = await fetch(reposURL);
-  const reposData = await reposRes.json();
+    const reposRes = await fetch(reposURL);
+    const reposData = await reposRes.json();
 
-  const result = { searchData, reposData };
-  return result;
+    const result = { searchData, reposData };
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const showProfile = (searchData, reposData) => {
@@ -27,13 +31,27 @@ const showProfile = (searchData, reposData) => {
   const $profileFollowers = document.querySelector('.followers-number');
   const $profileFollowing = document.querySelector('.following-number');
   const $profileReposNum = document.querySelector('.repos-number');
-  const $profileReposData = document.querySelector('.profile-info__repos');
+  const $profileReposBox = document.querySelector('.profile-info__repos-box');
+
+  const $reposContainer = document.createElement('ul');
+  reposData.forEach((repo) => {
+    const $repoItem = document.createElement('li');
+    $repoItem.className = 'repos-container';
+    $repoItem.className = 'repos-item';
+    $repoItem.textContent = repo.name;
+
+    $reposContainer.appendChild($repoItem);
+  });
 
   $profileImg.src = searchData.avatar_url;
   $profileName.textContent = searchData.name;
   $profileBio.textContent = searchData.bio;
   $profileFollowers.textContent = searchData.followers;
   $profileFollowing.textContent = searchData.following;
+  $profileReposNum.textContent = reposData.length;
+
+  $profileReposBox.innerHTML = '';
+  $profileReposBox.appendChild($reposContainer);
 };
 
 const handleSearch = async (e) => {
